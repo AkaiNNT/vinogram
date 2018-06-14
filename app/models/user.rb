9de4323b3ba5b5054fcafda738c9df1901218fa6
andy3520 <andy@vinova.sg>
2018-06-14 16:33:42 +0700

    fix double body conflict code selena

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,6 @@ class User < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
-  validates :contact_number,:presence => true,
-  :numericality => true,
-  :length => { :minimum => 10, :maximum => 15 }
-  
   def get_avatar
     if self.avatar.present?
       self.avatar.url
