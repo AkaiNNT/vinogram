3b88b4e9c90462194e35b11044aded596e4b97be
andy3520 <andy@vinova.sg>
2018-06-15 10:11:39 +0700

    generate devise admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
+  devise_for :users, path: 'users', controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
     registrations: 'users/registrations'
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
   get 'home/index'
   root to: "home#index"
   resources :users
+  devise_for :admins, path: 'admins', controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_scope :user do
+    get '/cms', to: 'admins/sessions#new'
+  end
 end
