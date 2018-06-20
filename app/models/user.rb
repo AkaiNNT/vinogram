761f0a5b6bae00ea404a05daa81a9a7271444352
selena1809 <selena@vinova.sg>
2018-06-20 15:55:05 +0700

    first deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,10 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/users/default_images.jpeg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-
   validates :contact_number,
   :numericality => true,
   :length => { :minimum => 10, :maximum => 15 },
   :allow_nil => true
-
   validates :full_name, :presence => true
   validates :full_name, :length => { :minimum => 3 },if: -> { self.full_name.present? }
 
